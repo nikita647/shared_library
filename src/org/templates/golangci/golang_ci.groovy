2045bba85fbe41e4a94f30516027acd50d88a9c3
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2025-03-21 20:03:12 +0530

    Update golang_ci.groovy

--- src/org/templates/golangci/golang_ci.groovy
+++ src/org/templates/golangci/golang_ci.groovy
@@ -4,12 +4,12 @@ import org.common.*
 import org.golang.*
 
 def call(String branch = 'main', String repoUrl, String gitPassword='' ){
-clean = new cleanworkspace()
-clone = new gitClone()
-compilation = new code_compilation()
+def clean = new cleanworkspace()
+def clone = new gitClone()
+def compilation = new code_compilation()
 
 clean.call()
-clone.call(branch,repoUrl,gitPassword)
+clone.call(branch, repoUrl, gitPassword)
 compilation.call()
   
 }
